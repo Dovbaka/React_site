474034032ab16ffcbbbb3b6bbe1795ebf0542bec
Dovbaka <dovbaka99@gmail.com>
2020-10-10 14:06:48 +0300

    Updates

---
+++ precache-manifest.76c4cc0b99c24bc02054c3aca734811c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482ae819b63be249c2d7d1f0b07c8235",
+    "url": "/React_site/index.html"
+  },
+  {
+    "revision": "f2a9aae567ccbbe76a0b",
+    "url": "/React_site/static/css/3.cb8389ca.chunk.css"
+  },
+  {
+    "revision": "6a84af1dccd85f922991",
+    "url": "/React_site/static/css/main.211d40a6.chunk.css"
+  },
+  {
+    "revision": "0fc37ddb30d3bb8219a2",
+    "url": "/React_site/static/js/2.55296c80.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/React_site/static/js/2.55296c80.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2a9aae567ccbbe76a0b",
+    "url": "/React_site/static/js/3.13bf950e.chunk.js"
+  },
+  {
+    "revision": "6a84af1dccd85f922991",
+    "url": "/React_site/static/js/main.ecb8be80.chunk.js"
+  },
+  {
+    "revision": "ccfc5b255b6e8fbbc6d6",
+    "url": "/React_site/static/js/runtime-main.d67735f0.js"
+  },
+  {
+    "revision": "39e1ce1a7e61d12a6b63cdb3800edb95",
+    "url": "/React_site/static/media/loading.39e1ce1a.svg"
+  }
+]);
